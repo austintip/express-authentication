6cc5676fa952c1e0a660db5fba4473d7a145437f
austintip <austin.d.tipograph@gmail.com>
2021-01-20 18:21:34 -0500

    clear, lazy, and juvenileconfig for passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
+
+// mounted at /auth
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
 });
+
+//Sign up POST route
+router.post('/signup', (req, res) => {
+  // findOrCreate a new user based on email
+  db.user.findOrCreate({
+    where: {
+      email: req.body.email
+    },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).then(([user, created]) => {
+    // If the user WAS created
+      if (created) {
+      //redirect to homepage or profile
+      console.log(`${user.name} was created! 🤢`);
+      res.redirect('/');
+    } else { // else (user wasn't created - there is a user at that email)
+        console.log(`${user.name} already exists!`);
+        res.redirect('/auth/signup');
+    //redirect to /auth/signup
+    }
+  }).catch(err => {
+    console.log('Bad news bears, theres an error!');
+    console.log(err);
+  })
+  // if there is an error it's probably a validation error, so we will return to /auth/signup
+});
+
+
+
 
 router.get('/login', (req, res) => {
   res.render('auth/login');
