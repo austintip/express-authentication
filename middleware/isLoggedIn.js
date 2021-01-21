f5cd6a2ee8afdd45783a023ed7e4efeabc168bf3
austintip <austin.d.tipograph@gmail.com>
2021-01-21 14:43:37 -0500

    stuff happeneing with css alerts

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
     if (!req.user) {
         console.log(`👹 You can't be here!`);
+        req.flash('error', 'You must be logged in to access that page');
         res.redirect('/auth/login');
     } else {
         next();
